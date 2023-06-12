16e80cdeeb53b8d28f72e20ab8d1e4f4425b5b61
Carlos vilares <car.vilares@gmail.com>
2023-06-12 23:15:43 +0000

    main function of guessing name add

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+def guessing_game():
+    """
+    Main function of the guessing game.
+    """
+    number=number.randomint(1,100)
+    """
+    The 'random.randint(a, b)' function is used to generate a random integer between 'a' and 'b'.
+    In this case, it generates a random number between 1 and 100 and assigns it to the 'number' variable.
+    """
+    attempts=0
+    """
+    This line attempts = 0 initializes the attempts variable and sets its initial value to 0.
+    """
