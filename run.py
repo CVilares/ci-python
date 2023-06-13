3b541c1cb29d24cf16e951a89946f76c3d158d03
Carlos vilares <car.vilares@gmail.com>
2023-06-13 00:05:25 +0000

    loop add to validate user guess and provide feedback

--- run.py
+++ run.py
@@ -7,12 +7,37 @@ def guessing_game():
     """
     Main function of the guessing game.
     """
-    number=number.randomint(1,100)
+    number = random.randint(1, 100)
     """
     The 'random.randint(a, b)' function is used to generate a random integer between 'a' and 'b'.
     In this case, it generates a random number between 1 and 100 and assigns it to the 'number' variable.
     """
-    attempts=0
+    attempts = 0
     """
     This line attempts = 0 initializes the attempts variable and sets its initial value to 0.
     """
+
+    while True:
+        # Get the user's guess
+        guess = input("Enter your guess: ")
+
+        
+
+        guess = int(guess)
+        attempts += 1
+
+        if guess < 1 or guess > 100:
+            # Check if the guess is within the valid range
+            print("Number must be between 1-100, please guess again.")
+        elif guess < number:
+            print("Too low. Try again!")
+        elif guess > number:
+            print("Too high. Try again!")
+        else:
+            # Correct guess
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
+
+    print("The game is over.")
+
+guessing_game()
