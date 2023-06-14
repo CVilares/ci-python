374cd9b35260de3431cfe0017f312a49f7c00afb
Carlos vilares <car.vilares@gmail.com>
2023-06-14 15:18:38 +0000

    function play game updated in order to introduce 3 level into the game

--- run.py
+++ run.py
@@ -7,6 +7,7 @@ def clear_screen():
     """
     print('\033c')
 
+
 def display_rules():
     """
     Display the rules of the game.
@@ -14,85 +15,56 @@ def display_rules():
     print("===================================")
     print("Guessing Game - Rules")
     print("===================================")
-    print("1. The computer will randomly choose a number between 1 and 100.")
+    print("1. The computer will randomly choose a number between 1 and 300.")
+    print("1. The computer will let you choose between 3 levels.")
     print("2. You have to guess the chosen number.")
-    print("3. The computer provides feedback:'Too low' or 'Too high'.")
+    print("3. The computer provides feedback: 'Too low' or 'Too high'.")
     print("4. Keep guessing until you guess the correct number.")
     print("===================================\n")
-    """
-    print the game rules
-    """
 
 
-def play_game():
-
+def play_game(A, B):
     """
     Play the guessing game.
     """
-    number = random.randint(1, 100)
-    """
-    Generate a random number between 1 and 100
-    """
+    number = random.randint(A, B)
+    # Generate a random number between A and B
     attempts = 0
 
     print("Welcome to the Guessing Game!")
-    print("Try to guess the number between 1 and 100.")
+    print(f"Try to guess the number between {A} and {B}.")
 
     while True:
-        """
-        this loop will repeat untill we get the correct guess
-        """
+        # This loop will repeat until we get the correct guess
         guess = input("Enter your guess: ")
-        """
-        Get the user's guess
-        """
+        # Get the user's guess
 
         if not guess.isdigit():
-            """
-             Check if the input is a valid number
-             """
+            # Check if the input is a valid number
             print("Invalid input. Please enter a valid number.")
             continue
 
         guess = int(guess)
-        """
-        tranform guess into a integer
-        """
+        # Transform guess into an integer
 
         attempts += 1
-        """
-        increase the attempts
-        """
+        # Increase the attempts
 
-        if guess < 1 or guess > 100:
-            """
-             Check if the guess is within the valid range
-            """
-            print("Number must be between 1-100, please guess again.")
+        if guess < A or guess > B:
+            print(f"Number must be between {A}-{B}, please guess again.")
         elif guess < number:
-            """
-            block is executed when the user's guess is less
-            than the randomly generated number
-            """
+            # Block is executed when the user's guess is less than the randomly generated number
             print("Too low. Try again!")
         elif guess > number:
-            """
-            block is executed when the user's guess is greater than
-            the randomly generated number
-            """
+            # Block is executed when the user's guess is greater than the randomly generated number
             print("Too high. Try again!")
         else:
-            """
-            block is executed when the user's guess is equal to the
-            randomly generated number
-            """
-            print(f"Congratulations!You took {attempts} attempts.")
+            # Block is executed when the user's guess is equal to the randomly generated number
+            print(f"Congratulations! You took {attempts} attempts.")
             break
 
     print("The game is over.")
-    """
-    When the loop is over this message will be printed
-    """
+    # When the loop is over, this message will be printed
 
 
 def start_game():
@@ -103,42 +75,42 @@ def start_game():
     print("Welcome to the Guessing Game!")
 
     while True:
-        """
-        Get the user's choice
-        """
+        # Get the user's choice
         print("")
         print("Press 'R' to read the rules.")
         print("Press 'P' to play the game.")
         print("Press 'Q' to quit the game.")
         print("")
-        
-        choice = input("Enter your choice:").upper().strip()
 
+        choice = input("Enter your choice:").upper().strip()
+        print("")
         if choice == "R":
-            """
-            Display the rules
-            """
             display_rules()
         elif choice == "P":
-            """
-             Play the game
-            """
-            play_game()
+            print("")
+            print("Select the level:")
+            print("")
+            print("'E' to Easy (1-50)")
+            print("'M' to Medium (1-100)")
+            print("'P' to Pro (1-300)")
+            print("")
+            level_choice = input("Enter the level number: ").upper().strip()
+
+            if level_choice == "E":
+                play_game(1, 50)
+            elif level_choice == "M":
+                play_game(1, 100)
+            elif level_choice == "P":
+                play_game(1, 300)
+            else:
+                print("Invalid level choice. Please try again.")
+
         elif choice == "Q":
-            """
-            Quit the game
-            """
             print("Exiting the game.")
             break
         else:
             print("")
             print("Invalid choice. Please try again.")
-            
-        
-        
 
 
 start_game()
-
-
-
